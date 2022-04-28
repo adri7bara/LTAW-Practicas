6f7baddef36acef8cdbdb54dd845e8e6d52b53d6
Adrian Barahona <a.barahona.2016@alumnos.urjc.es>
2022-04-28 12:11:51 +0200

    js

---
+++ P1/tienda.js
@@ -0,0 +1,11 @@
+
+//--  Acceso a todos los elementos del módulo http.
+const http = require('http');
+//-- Importar modulo fs
+const fs = require('fs');
+//-- Importar url
+const url = require('url');
+//-- Importar el modulo path  
+var path = require('path');
+//-- El servidor debe escuchar en el puerto 9090
+const PUERTO = 9090;
